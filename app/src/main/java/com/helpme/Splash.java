cee3ee70ab2b0542cd424308f99135b50659fd4a
felipetorreslinux <felipe.torres.oficial@hotmail.com>
2018-07-31 23:03:03 -0300

    app

--- app/src/main/java/com/helpme/Splash.java
+++ app/src/main/java/com/helpme/Splash.java
@@ -1,13 +1,20 @@
 package com.helpme;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.view_splash );
+
+
     }
+
 }
